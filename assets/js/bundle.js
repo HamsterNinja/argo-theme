501dde56d6bf616b49b568730725cb0bb5e75d64
HamsterNinja <oohamsterninjaoo@gmail.com>
2020-01-21 18:37:42 +0300

    адаптивка меню доставки

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fa377bec29b0a4b7395f"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "13941a095875f7f33923"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
